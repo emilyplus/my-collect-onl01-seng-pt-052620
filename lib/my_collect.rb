fd50538f3e250249aff3b96ed1b0e0f74e2ac653
Emily Jones <emjones829@gmail.com>
2020-06-07 17:27:35 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,19 +1,12 @@
-
-
 def my_collect(array)
   
   i = 0
+  
   collection = []
   while i < array.length
     collection[i] = yield array[i]
     i += 1
   end
  
-  counter = 0
-  end
-  my_collect(empty) do |x|
-    x {counter += 1}
+  return collection
 end
-
-my_collect(langauges) do |language|
-    language.upcase
